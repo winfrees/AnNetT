b1d882dc58671e427ee99c8f1085909d8c7e47cb
winfrees <winfrees@IN-MDEP-120247.ads.iu.edu>
2014-03-11 15:05:47 -0400

    Merge origin/master
    Conflicts:
    	README.md

--- src/main/java/Analyze_Network/Analyze_Network.java
+++ src/main/java/Analyze_Network/Analyze_Network.java
@@ -189,17 +189,17 @@ public class Analyze_Network implements PlugInFilter {
         return rtResult;
         }
         
-        public static void main(String[] args) {
-//		// set the plugins.dir property to make the plugin appear in the Plugins menu
-//		Class<?> clazz = Analyze_Network.class;
-//		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
-//		String pluginsDir = url.substring(5, url.length() - clazz.getName().length() - 6);
-//		System.setProperty("plugins.dir", pluginsDir);
-
-//		// start ImageJ
-//		new ImageJ();
+//        public static void main(String[] args) {
+////		// set the plugins.dir property to make the plugin appear in the Plugins menu
+////		Class<?> clazz = Analyze_Network.class;
+////		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
+////		String pluginsDir = url.substring(5, url.length() - clazz.getName().length() - 6);
+////		System.setProperty("plugins.dir", pluginsDir);
 //
-//		// run the plugin
-//		IJ.runPlugIn(clazz.getName(), "");
-	}
+////		// start ImageJ
+////		new ImageJ();
+////
+////		// run the plugin
+////		IJ.runPlugIn(clazz.getName(), "");
+//	}
 }
